bd9f1044b3f3cd1f41493959515450197647e0d8
r-svp26 <riteshk6341@gmail.com>
2021-06-23 00:07:39 +0530

    Added: Evaluation of an Employee monthly wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,23 +1,34 @@
 #!/bin/bash -x
 
+#CONSTANSTS
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 EMP_RATE_PER_HR=20;
+NUM_WORKING_DAY=20;
 
-empCheck=$((RANDOM%3))
+#VARIABLES
+totalSalary=0
 
-case $empCheck in
-	$IS_FULL_TIME)
-		empHrs=16
-		;;
-	$IS_PART_TIME)
-		empHrs=8
-		;;
-	*)
-		empHrs=0
-		;;
+for (( day=1; day<=NUM_WORKING_DAY; day++ ))
+do
+	empCheck=$((RANDOM%3))
+		case $empCheck in
+			$IS_FULL_TIME)
+				empHrs=16
+				;;
+			$IS_PART_TIME)
+				empHrs=8
+				;;
+			*)
+				empHrs=0
+				;;
 
-esac
+			esac
 
-salary=$((empHrs*EMP_RATE_PER_HR))
+			salary=$(( $empHrs*$EMP_RATE_PER_HR ))
+			totalSalary=$(( $totalSalary+$salary ))
+
+done
+
 echo $salary
+echo $totalSalary
