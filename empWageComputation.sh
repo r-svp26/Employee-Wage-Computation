ac8f952579a5e044ac747351f5fb8a0f10ed9126
r-svp26 <riteshk6341@gmail.com>
2021-06-22 22:05:48 +0530

    Added: Evaluation of an employee daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -x
 
+EMP_RATE_PER_HR=20;
+FULL_TIME_HR=8;
+
 empCheck=$((RANDOM%2))
+isPresent=1;
 
-IS_PRESENT=1
-
-if [ $empCheck -eq IS_PRESENT ]
-	echo "Employee is present."
+if [ $isPresent -eq $empCheck ]
+then
+	salary=$(( $EMP_RATE_PER_HR*$FULL_TIME_HR ))
 else 
-	echo "Employee is absent."
+	salary=0;
 fi 
+
+echo "Daily Wage of an Employee= " $salary
