3bef854fc647e70795f38feabd49da8c1967cb06
r-svp26 <riteshk6341@gmail.com>
2021-06-23 00:59:04 +0530

    Added: Employee wage with max hrs condition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,30 +5,30 @@ IS_PART_TIME=1;
 IS_FULL_TIME=2;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAY=20;
+MAX_HRS_IN_MONTH=100;
 
 #VARIABLES
-totalSalary=0
+totalEmpHrs=0
+totalWorkingDays=0
 
-for (( day=1; day<=NUM_WORKING_DAY; day++ ))
+while [[ $totalEmpHrs -lt MAX_HRS_IN_MONTH && 
+	$totalWorkingDays -lt $NUM_WORKING_DAY ]]
 do
+	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
-		case $empCheck in
-			$IS_FULL_TIME)
-				empHrs=16
-				;;
-			$IS_PART_TIME)
-				empHrs=8
-				;;
-			*)
-				empHrs=0
-				;;
-
-			esac
-
-			salary=$(( $empHrs*$EMP_RATE_PER_HR ))
-			totalSalary=$(( $totalSalary+$salary ))
-
+	case $empCheck in
+		$IS_FULL_TIME)
+			empHrs=16
+			;;
+		$IS_PART_TIME)
+			empHrs=8
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	totalEmpHrs=$(($totalEmpHrs+empHrs))
 done
 
-echo $salary
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
 echo $totalSalary
