1ffec80d2909242afe67e796e689e6bf658487e4
r-svp26 <riteshk6341@gmail.com>
2021-06-23 06:30:29 +0530

    Refactored: Used getWorkHrs function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,33 +1,39 @@
 #!/bin/bash -x
 
-#CONSTANSTS
+#constants
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAY=20;
 MAX_HRS_IN_MONTH=100;
 
-#VARIABLES
+#variables
 totalEmpHrs=0
 totalWorkingDays=0
+
+function getWorkHrs() {
+        case $1 in
+                $IS_FULL_TIME)
+                        empHrs=16
+                        ;;
+                $IS_PART_TIME)
+                        empHrs=8
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        echo  $empHrs;
+}
 
 while [[ $totalEmpHrs -lt MAX_HRS_IN_MONTH && 
 	$totalWorkingDays -lt $NUM_WORKING_DAY ]]
 do
 	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$IS_FULL_TIME)
-			empHrs=16
-			;;
-		$IS_PART_TIME)
-			empHrs=8
-			;;
-		*)
-			empHrs=0
-			;;
-	esac
+	empHrs="$( getWorkHrs $empCheck )"
 	totalEmpHrs=$(($totalEmpHrs+empHrs))
+
 done
 
 totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
